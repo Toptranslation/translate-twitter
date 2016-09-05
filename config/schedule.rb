9df157242afd483fad5b71dae8f7103b99178419
Waldemar <wg@toptranslation.com>
2016-09-05 17:56:10 +0200

    fix schedule.rb to work in Docker

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,10 @@
 set :output, File.expand_path("../../log/cron_log.log", __FILE__)
+set :job_template, "bash -c ':job'"
+
 env :PATH, ENV['PATH']
+env :GEM_HOME, ENV['GEM_HOME']
+env :BUNDLE_APP_CONFIG, ENV['BUNDLE_APP_CONFIG']
+
 
 every 2.minutes do
    rake "twitter:tweet_translation"
